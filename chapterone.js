63c967a914ce49b21fe6678c2195ae71251b49a0
sparksflyupwards <saad.jamil.khan@gmail.com>
2020-06-10 18:44:32 -0400

    first commit chapter 3 work

---
+++ chapterone.js
@@ -0,0 +1,45 @@
+let min  = (val1, val2) =>{
+    return val1 < val2 ? val1 : val2;
+}
+
+console.log(min(2,32))
+
+
+let isEven = (num) => {
+    if(num==0){
+        return "even"
+    }
+    else if (num == 1 ) {
+        return "odd"
+    }
+    else {
+        return isEven(num - 2)
+    }
+}
+
+console.log(isEven(21))
+
+let countB = (str) => {
+    count = 0;
+    for(letter of str){
+        if (letter == "B"){
+            count ++;
+        }
+    }
+    return count;
+}
+
+console.log(countB("BBBbbbBBB"))
+
+let countChar  = (str, char) =>{
+
+    count = 0;
+    for(let i=0; i<str.length; i++){
+        if(str[i] == char){
+            count ++
+        }
+    }
+
+    return count;
+}
+console.log(countChar("BBBbbbBBB","b"))
